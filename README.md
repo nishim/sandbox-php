c6b0f09ab72134dd6c50598aa54cbb3e6c6da973
nishim <yuji@liclab.com>
2022-03-18 20:58:04 +0900

    carbon

---
+++ README.md
@@ -0,0 +1,1 @@
+# Sandbox for PHP & PHP Libraries.
---
+++ carbon.php
@@ -0,0 +1,17 @@
+<?php
+require 'vendor/autoload.php';
+
+use Carbon\Carbon;
+
+echo "今日の0時ちょうど\n";
+echo Carbon::today()->format('Y-m-d H:i:s');
+echo "\n\n";
+echo "7日後の23:59:59\n";
+echo Carbon::now()->addDays(7)->format('Y-m-d 23:59:59');
+echo "\n\n";
+echo "今月最初の日の0時ちょうど\n";
+echo Carbon::now()->firstOfMonth()->format('Y-m-d H:i:s');
+echo "\n\n";
+echo "今月最後の日の23:59:59\n";
+echo Carbon::now()->endOfMonth()->format('Y-m-d H:i:s');
+echo "\n\n";
